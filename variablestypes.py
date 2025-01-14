565face5cf8fc12f3ce80c657358dff648330259
teuzed <georguiazelpachecos@gmail.com>
2025-01-13 21:04:42 -0500

    Add variable type examples and initialize new variables in variablestypes.py

--- variablestypes.py
+++ variablestypes.py
@@ -1,12 +1,17 @@
-# x = True	                            bool 
-# x = ["apple", "banana", "cherry"]	    list
-# 
+#   x = True | False	                    bool 
+#   x = ["apple", "banana", "cherry"]	    list
+#   x = "Hello World"	                    str	
+#   x = 20	                                int	
+#   x = 20.5	                            float
 
 
-
-
-
+y = True
+marcas = ["Toyota","Volvo","Nissan","Audi"]
+frutas = ["Manzana", "Pera","Platano"]
+numeros = [ 1, 2 ,3 , 4, 5, 6 , 7]
+texto = "Hello World"
 x = 5
+peso = 10.4
 
 
-print(type(x))
+print(type(peso))
