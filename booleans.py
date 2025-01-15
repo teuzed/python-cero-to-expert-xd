9f6014a222a82942d335ab286fb33f5b7429f19d
teuzed <georguiazelpachecos@gmail.com>
2025-01-14 20:48:23 -0500

    [feat]: condicionales

---
+++ booleans.py
@@ -0,0 +1,13 @@
+# bool  ->  False | True
+a = 200
+b = 33
+
+print(b > a)
+
+if b > a:
+  print("b es mayor que  a") 
+else:
+  print("b es menor que a")
+
+
+
---
+++ booleans1.py
@@ -0,0 +1,12 @@
+
+ana_edad = 15
+juan_edad = 20
+
+print(ana_edad > juan_edad)
+
+if ana_edad > juan_edad:
+    print("Ana es mayor que Juan")
+else:
+    print("Juan es mayor de Ana")
+
+
